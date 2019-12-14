882e7bf722d9760fb9ec5bed31f65452665e1a1c
Sardor Dushamov <dsq2190@gmail.com>
2019-12-14 08:35:47 +0500

    commited

---
+++ src/MyWidget.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sardor Dushamov <t.me/SardorOga>
+ * Date: 12/14/19
+ * Time: 8:28 AM
+ */
+
+namespace sardoroga\hello;
+
+
+class MyWidget 
+{
+    public static function sayhello()
+    {
+        return "salom dunyo";
+    }
+}
